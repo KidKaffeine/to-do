1e906cb13b23f3ae0dac50ceb9d9988fae73080d
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-11-15 11:43:39 +0000

    created and tested routes on db CRUD operations

--- backend/routes/routing.js
+++ backend/routes/routing.js
@@ -1,37 +1,62 @@
 const express = require("express")
 const router = express.Router()
+const Tasks = require("../db/models")
 
-router.get("/getAll", (req, res) => {
+router.get("/getAll", async (req, res) => {
     try {
-        res.status(200).json({ message: "Tasks are here" })
+        const tasks = await Tasks.find()
+
+        if (!tasks) {
+            res.status(404).json({ message: "Could not get tasks." })
+        }
+
+        res.status(200).json(tasks)
     } catch (error) {
+
         console.log(req.err)
-        throw new Error({ message: "Could not find tasks" })
+        throw new Error({ message: "Could not find tasks." })
     }
 })
 
 router.post("/create", async (req, res) => {
     try {
-        res.status(201).json({ message: "Task created" })
+        const newTask = await Tasks.create({ task: req.body.task })
+
+        if (!req.body.task) {
+            res.status(400).json({ message: "Please add a task." })
+        }
+
+        res.status(201).json(newTask)
     } catch (error) {
         console.log(req.err)
-        throw new Error({ message: "Could not create tasks" })
+        throw new Error({ message: "Could not create tasks." })
     }
 })
 
-router.put("/update/:id", async (req, res) => {
+router.put("/update/:_id", async (req, res) => {
     try {
-        res.status(200).json({ message: "Task updated" })
+        if (!req.params._id || !req.body.newTask) {
+            res.status(400).json({ message: "Please update or select a task." })
+        }
+
+        const updatedTask = await Tasks.findOneAndUpdate({ _id: req.params._id, task: req.body.newTask })
+
+        res.status(200).json(updatedTask)
     } catch (error) {
         console.log(req.err)
-        throw new Error({ message: "Could not update task" })
+        throw new Error({ message: "Could not update task." })
     }
 })
 
-router.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:_id", async (req, res) => {
     try {
-        console.log(`${req.params.id}`)
-        res.status(200).json({ message: "Task deleted" })
+        if (!req.params._id) {
+            res.status(400).json({ message: "Please select a task to delete." })
+        }
+
+        await Tasks.findOneAndDelete({ _id: req.params._id })
+
+        res.status(200).json({ message: `Deleted task ${req.params._id}` })
     } catch (error) {
         console.log(req.err)
         throw new Error({ message: "Could not delete task" })
